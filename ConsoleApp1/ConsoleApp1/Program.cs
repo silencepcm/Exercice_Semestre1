10de2f7d506f9705ba592eda2d92cdd60ead9595
silencepcm <igor.kiselev090@gmail.com>
2021-09-20 12:39:08 +0300

    Added VS Project + Did Exercice

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Exercice
+    {
+        public const double PI = 3.1415926535897931;
+
+
+        public int euclid_rest(int a, int b)
+        {
+            return a % b;
+        }
+        public double volume_sphere(double radius)
+        {
+            return 4 / 3 * PI * Math.Pow(radius, 3);
+        }
+
+        public double discount_price(double price, int int_discount)
+        {
+            double discount = Convert.ToDouble(int_discount);
+            return price - price * discount / 100;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercice = new Exercice();
+
+            Console.WriteLine("Hello! Let's test)\n");
+
+            Console.WriteLine("| Euclid_rest |");
+            Console.WriteLine("Enter 2 numbers:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result:\n" + exercice.euclid_rest(a, b) + "\n");
+
+            Console.WriteLine("| Volume_sphere |");
+            Console.WriteLine("Enter radius:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Result:\n" + exercice.volume_sphere(radius) + "\n");
+
+            Console.WriteLine("| discount_price |");
+            Console.WriteLine("Enter price and discount:");
+            double price = Convert.ToDouble(Console.ReadLine());
+            int discount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result:\n" + exercice.discount_price(price, discount));
+        }
+    }
+}
