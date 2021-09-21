18933d77ff10edf03929280d3f9f103ccaecf5dd
silencepcm <igor.kiselev090@gmail.com>
2021-09-21 11:21:52 +0300

    Ex5 added

--- ConsoleApp1/ConsoleApp1/Ex5.cs
+++ ConsoleApp1/ConsoleApp1/Ex5.cs
@@ -6,5 +6,19 @@ namespace ConsoleApp1
 {
     class Ex5
     {
+        public void InitShowArray(int[,] arr)
+        {
+            var rand = new Random();
+            for(int i=0; i< arr.GetLength(0); ++i)
+            {
+                Console.Write("|");
+                for (int j = 0; j < arr.GetLength(1); ++j)
+                {
+                    arr[i, j] = rand.Next(0, 9);
+                    Console.Write(arr[i, j] + "|");
+                }
+                Console.Write("\n");
+            }
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,65 +4,74 @@ namespace ConsoleApp1
 {
     class Program
     {
-
-        static public void ShowArray(int [] arr)
-        {
-            foreach (var item in arr)
-            {
-                Console.WriteLine(item.ToString());
-            }
-        }
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello! Let's test)\n");
-            /*
-            var exercice1 = new Ex1();
-            Console.WriteLine("| Euclid rest |");
-            Console.WriteLine("Enter 2 numbers:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Result:\n" + exercice1.EuclidRest(a, b) + "\n");
-
-            var exercice2 = new Ex2();
-            Console.WriteLine("| Volume sphere |");
-            Console.WriteLine("Enter radius:");
-            double radius = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Result:\n" + exercice2.VolumeSphere(radius) + "\n");
-            
-            var exercice3 = new Ex3();
-            Console.WriteLine("| Discount price |");
-            Console.WriteLine("Enter price and discount:");
-            double price = Convert.ToDouble(Console.ReadLine());
-            int discount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Result:\n" + exercice3.DiscountPrice(price, discount));
-            */
-
-            var exercice4 = new Ex4();
             var rand = new Random();
-            Console.WriteLine("| Arrays Filtre |");
-            // int[] arr1, int[] arr2, int NumberSeuil, int rabais
-            int[] arr1;
-            int[] arr2;
-            arr1 = new int[10];
-            arr2 = new int[10];
-
-            for(int i = 0; i < 10; i++)
+            Console.WriteLine("Hello! Let's test)\n");
+            Console.WriteLine("Select exercise number: 1,2,3,4,5 \n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
             {
-                arr1[i] = rand.Next(0, 101);
-                arr2[i] = rand.Next(0, 101);
-            }
-            int NumberSeuil = rand.Next(0, 101);
-            int rabais = rand.Next(0, 101);
-            Console.WriteLine("Arr1:");
-            Console.WriteLine(string.Join(" ", arr1));
-            Console.WriteLine("Arr2:");
-            Console.WriteLine(string.Join(" ", arr2));
-            Console.WriteLine("NumberSeuil: " + NumberSeuil);
-            Console.WriteLine("rabais: " + rabais);
+                case 1:
+                    var exercice1 = new Ex1();
+                    Console.WriteLine("| Euclid rest |");
+                    Console.WriteLine("Enter 2 numbers:");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Result:\n" + exercice1.EuclidRest(a, b) + "\n");
+                    break;
 
-            Console.WriteLine("Result:");
-            Console.WriteLine(string.Join(" ", exercice4.ArrayFilter(arr1, arr2, NumberSeuil, rabais)));
+                case 2:
+                    var exercice2 = new Ex2();
+                    Console.WriteLine("| Volume sphere |");
+                    Console.WriteLine("Enter radius:");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Result:\n" + exercice2.VolumeSphere(radius) + "\n");
+                    break;
+
+                case 3:
+                    var exercice3 = new Ex3();
+                    Console.WriteLine("| Discount price |");
+                    Console.WriteLine("Enter price and discount:");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    int discount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Result:\n" + exercice3.DiscountPrice(price, discount));
+                    break;
+
+                case 4:
+                    var exercice4 = new Ex4();
+                    Console.WriteLine("| Arrays Filtre |");
+                    int[] arr1 = new int[10];
+                    int[] arr2 = new int[10];
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        arr1[i] = rand.Next(0, 101);
+                        arr2[i] = rand.Next(0, 101);
+                    }
+                    int NumberSeuil = rand.Next(0, 101);
+                    int rabais = rand.Next(0, 101);
+                    Console.WriteLine("Arr1:");
+                    Console.WriteLine(string.Join(" ", arr1));
+                    Console.WriteLine("Arr2:");
+                    Console.WriteLine(string.Join(" ", arr2));
+                    Console.WriteLine("NumberSeuil: " + NumberSeuil);
+                    Console.WriteLine("rabais: " + rabais);
+
+                    Console.WriteLine("Result:");
+                    Console.WriteLine(string.Join(" ", exercice4.ArrayFilter(arr1, arr2, NumberSeuil, rabais)));
+                    break;
+
+                case 5:
+                    int arr5Len1 = rand.Next(5, 11);
+                    int arr5Len2 = rand.Next(5, 11);
+                    int[,] arr5 = new int[arr5Len1, arr5Len2];
+                    var exercice5 = new Ex5();
+                    exercice5.InitShowArray(arr5);
+                    break;
+
+                default: Console.WriteLine("ERROR"); break;
+            }
 
         }
     }
